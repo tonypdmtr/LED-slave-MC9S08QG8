521ed8146ab2353c0716ceca2d5fb1254d4b48f8
Tony Papadimitriou <tonyp@acm.org>
2019-11-03 00:44:56 +0200

    Adapted to ASM8 Macro Assembler http://www.aspisys.com/asm8.htm
    Automatic calculation of some timings based on bus frequency.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # LED-slave-MC9S08QG8
+
+Adapted to [ASM8 Macro Assembler](http://www.aspisys.com/asm8.htm)
+
+Automatic calculation of some timings based on bus frequency.
--- main.asm
+++ main.asm
@@ -1,42 +1,41 @@
-;**********************************************************************
+;*******************************************************************************
 ; EE465: Microcontroller Applications
 ; Montana State University, Bozeman
 ; by Aaron Jense
 ;
-; Slave HCS08 receives keypress through I2C and flashes LED bar pattern                                          *
-;**********************************************************************		
-			INCLUDE 'derivative.inc'
-      
-            XDEF _Startup, main
-            XREF __SEG_END_SSTACK
-            XREF SETUP_TPM, SETUP_IIC_SLAVE
-            XREF FLASH_LED_ARRAY
-			XREF _VIIC, _VTPMOVF		
-MY_ZEROPAGE: SECTION  SHORT
-ROM_VAR: SECTION	
+; Slave HCS08 receives keypress through I2C and flashes LED bar pattern
+;*******************************************************************************
+                    #Uses     qg8.inc
+                    #Uses     iic.sub
+                    #Uses     led_bar.sub
+                    #Uses     tpm.sub
+                    #Uses     util.sub
+;*******************************************************************************
 
-MyCode:     SECTION
-main:
-_Startup:
-            LDHX #__SEG_END_SSTACK ; initialize the stack pointer
-            TXS
-			CLI	; enable interrupts
-			JSR	SETUP_SOPT1
-			JSR	SETUP_IO
-			JSR SETUP_IIC_SLAVE
-			JSR SETUP_TPM
-			BRA mainLoop
-mainLoop:
-			JSR FLASH_LED_ARRAY
-            BRA	mainLoop                 	
-SETUP_SOPT1:
-			LDA SOPT1 ; disable watchdog
-			AND #%01111111
-			STA SOPT1		
-			RTS
-SETUP_IO:
-			LDA	#%11111111 ; PTBD set to output for LED BAR
-			STA PTBDD
-			LDA #%00000000 
-			STA PTADD
-			RTS
+_Startup            proc
+                    ldhx      #STACKTOP           ; initialize the stack pointer
+                    txs
+                    cli                           ; enable interrupts
+                    bsr       Setup_SOPT1
+                    bsr       Setup_IO
+                    jsr       Setup_IIC_SLAVE
+                    bsr       Setup_TPM
+MainLoop@@          jsr       FLASH_LED_ARRAY
+                    bra       MainLoop@@
+
+;*******************************************************************************
+
+Setup_SOPT1         proc
+                    lda       SOPT1               ; disable watchdog
+                    and       #%01111111
+                    sta       SOPT1
+                    rts
+
+;*******************************************************************************
+
+Setup_IO            proc
+                    lda       #%11111111          ; PTBD set to output for LED BAR
+                    sta       PTBDD
+                    lda       #%00000000
+                    sta       PTADD
+                    rts
